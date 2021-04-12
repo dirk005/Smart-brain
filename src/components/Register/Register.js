7beb53b05e8aa8c12eb1790af1e8eb64f2926038
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-04-12 10:47:01 +0200

    Chage env

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 
   onSubmitSignIn = () => {
     let url = "http://localhost:3000";
-    if (process.env.SITE_LIVE === "development") {
+    if (process.env.SITE_LIVE === "production") {
       url = "https://git.heroku.com/powerful-crag-88676.git";
     }
     fetch(`${url}/register`, {
